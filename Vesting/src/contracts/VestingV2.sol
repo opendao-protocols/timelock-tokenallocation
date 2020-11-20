c9e328d642ba402867fdeb72cd2c7899bef8be2b
sajet006 <41197088+sajet006@users.noreply.github.com>
2020-11-20 11:35:32 +0530

    Update vesting for beneficiary.

--- Vesting/src/contracts/VestingV2.sol
+++ Vesting/src/contracts/VestingV2.sol
@@ -265,6 +265,7 @@ contract Comp {
 
     /// @notice The number of checkpoints for each account
     mapping (address => uint32) public numCheckpoints;
+    
 
     /// @notice The EIP-712 typehash for the contract's domain
     bytes32 public constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
@@ -531,106 +532,15 @@ contract Comp {
     }
 }
 
-contract CloneFactory {
-
-    function createClone(address target) internal returns (address result) {
-        bytes20 targetBytes = bytes20(target);
-        assembly {
-            let clone := mload(0x40)
-            mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
-            mstore(add(clone, 0x14), targetBytes)
-            mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-            result := create(0, clone, 0x37)
-        }
-    }
-}
-
-
-contract VestingDepositAccount {
-
-    /// @notice the controlling parent vesting contract
-    address public controller;
-
-    /// @notice beneficiary who tokens will be transferred to
-    address public beneficiary;
-
-    /// @notice ERC20 token that is vested (extended with a delegate function)
-    Comp public token;
-
-    /**
-     * @notice Using a minimal proxy contract pattern initialises the contract and sets delegation
-     * @dev initialises the VestingDepositAccount (see https://eips.ethereum.org/EIPS/eip-1167)
-     * @dev only controller
-     */
-    function init(address _tokenAddress, address _controller, address _beneficiary) external {
-        require(controller == address(0), "VestingDepositAccount::init: Contract already initialized");
-        token = Comp(_tokenAddress);
-        controller = _controller;
-        beneficiary = _beneficiary;
-
-        // sets the beneficiary as the delegate on the token
-        token.delegate(beneficiary);
-    }
-
-    /**
-     * @notice Transfer tokens vested in the VestingDepositAccount to the beneficiary
-     * @param _amount amount of tokens (in wei)
-     * @dev only controller
-     */
-    function transferToBeneficiary(uint256 _amount) external returns (bool) {
-        require(msg.sender == controller, "VestingDepositAccount::transferToBeneficiary: Only controller");
-        return token.transfer(beneficiary, _amount);
-    }
-
-    /**
-     * @notice Allows the beneficiary to be switched on the VestingDepositAccount and sets delegation
-     * @param _newBeneficiary address to receive tokens once switched
-     * @dev only controller
-     */
-    function switchBeneficiary(address _newBeneficiary) external {
-        require(msg.sender == controller, "VestingDepositAccount::switchBeneficiary: Only controller");
-        beneficiary = _newBeneficiary;
-
-        // sets the new beneficiary as the delegate on the token
-        token.delegate(_newBeneficiary);
-    }
-}
 /// @author BlockRocket
-contract VestingContract is CloneFactory, ReentrancyGuard {
+contract VestingContractWithoutDelegation is ReentrancyGuard {
     using SafeMath for uint256;
 
     /// @notice event emitted when a vesting schedule is created
-    event ScheduleCreated(address indexed _beneficiary, uint256 indexed _amount);
+    event ScheduleCreated(address indexed _beneficiary);
 
     /// @notice event emitted when a successful drawn down of vesting tokens is made
-    event DrawDown(address indexed _beneficiary, uint256 indexed _amount, uint256 indexed _time);
-
-    /// @notice struct to define the total amount vested (this never changes) and the associated deposit account
-    struct Schedule {
-        uint256 amount;
-        VestingDepositAccount depositAccount;
-    }
-
-    /// @notice owner address set on construction
-    address public owner;
-
-    /// @notice beneficiary to schedule mapping. Note beneficiary address can not be reused
-    mapping(address => Schedule) public vestingSchedule;
-
-    /// @notice cumulative total of tokens drawn down (and transferred from the deposit account) per beneficiary
-    mapping(address => uint256) public totalDrawn;
-
-    /// @notice last drawn down time (seconds) per beneficiary
-    mapping(address => uint256) public lastDrawnAt;
-
-    /// @notice set when updating beneficiary (via owner) to indicate a voided/completed schedule
-    mapping(address => bool) public voided;
-
-    /// @notice ERC20 token we are vesting
-    IERC20 public token;
-
-    /// @notice the blueprint deposit account to clone using CloneFactory (https://eips.ethereum.org/EIPS/eip-1167)
-    address public baseVestingDepositAccount;
+    event DrawDown(address indexed _beneficiary, uint256 indexed _amount);
 
     /// @notice start of vesting period as a timestamp
     uint256 public start;
@@ -641,28 +551,38 @@ contract VestingContract is CloneFactory, ReentrancyGuard {
     /// @notice cliff duration in seconds
     uint256 public cliffDuration;
 
+    /// @notice owner address set on construction
+    address public owner;
+
+    /// @notice amount vested for a beneficiary. Note beneficiary address can not be reused
+    mapping(address => uint256) public vestedAmount;
+
+    /// @notice cumulative total of tokens drawn down (and transferred from the deposit account) per beneficiary
+    mapping(address => uint256) public totalDrawn;
+
+    /// @notice last drawn down time (seconds) per beneficiary
+    mapping(address => uint256) public lastDrawnAt;
+    
+    /// @notice set when updating beneficiary (via owner) to indicate a voided/completed schedule
+    mapping(address => bool) public voided;
+    
+    /// @notice ERC20 token we are vesting
+    IERC20 public token;
+
     /**
      * @notice Construct a new vesting contract
      * @param _token ERC20 token
-     * @param _baseVestingDepositAccount address of the VestingDepositAccount to clone
      * @param _start start timestamp
      * @param _end end timestamp
      * @param _cliffDurationInSecs cliff duration in seconds
      * @dev caller on constructor set as owner; this can not be changed
      */
-    constructor(
-        IERC20 _token,
-        address _baseVestingDepositAccount,
-        uint256 _start,
-        uint256 _end,
-        uint256 _cliffDurationInSecs
-    ) public {
+    constructor(IERC20 _token, uint256 _start, uint256 _end, uint256 _cliffDurationInSecs) public {
         require(address(_token) != address(0), "VestingContract::constructor: Invalid token");
         require(_end >= _start, "VestingContract::constructor: Start must be before end");
 
         token = _token;
         owner = msg.sender;
-        baseVestingDepositAccount = _baseVestingDepositAccount;
 
         start = _start;
         end = _end;
@@ -670,81 +590,43 @@ contract VestingContract is CloneFactory, ReentrancyGuard {
     }
 
     /**
+     * @notice Create new vesting schedules in a batch
+     * @notice A transfer is used to bring tokens into the VestingDepositAccount so pre-approval is required
+     * @param _beneficiaries array of beneficiaries of the vested tokens
+     * @param _amounts array of amount of tokens (in wei)
+     * @dev array index of address should be the same as the array index of the amount
+     */
+    function createVestingSchedules(
+        address[] calldata _beneficiaries,
+        uint256[] calldata _amounts
+    ) external returns (bool) {
+        require(msg.sender == owner, "VestingContract::createVestingSchedules: Only Owner");
+        require(_beneficiaries.length > 0, "VestingContract::createVestingSchedules: Empty Data");
+        require(
+            _beneficiaries.length == _amounts.length,
+            "VestingContract::createVestingSchedules: Array lengths do not match"
+        );
+
+        bool result = true;
+
+        for(uint i = 0; i < _beneficiaries.length; i++) {
+            address beneficiary = _beneficiaries[i];
+            uint256 amount = _amounts[i];
+            _createVestingSchedule(beneficiary, amount);
+        }
+
+        return result;
+    }
+
+    /**
      * @notice Create a new vesting schedule
      * @notice A transfer is used to bring tokens into the VestingDepositAccount so pre-approval is required
-     * @notice Delegation is set for the beneficiary on the token during schedule creation
      * @param _beneficiary beneficiary of the vested tokens
      * @param _amount amount of tokens (in wei)
      */
     function createVestingSchedule(address _beneficiary, uint256 _amount) external returns (bool) {
         require(msg.sender == owner, "VestingContract::createVestingSchedule: Only Owner");
-        require(_beneficiary != address(0), "VestingContract::createVestingSchedule: Beneficiary cannot be empty");
-        require(_amount > 0, "VestingContract::createVestingSchedule: Amount cannot be empty");
-
-        // Ensure only one per address
-        require(
-            vestingSchedule[_beneficiary].amount == 0,
-            "VestingContract::createVestingSchedule: Schedule already in flight"
-        );
-
-        // Set up the vesting deposit account for the _beneficiary
-        address depositAccountAddress = createClone(baseVestingDepositAccount);
-        VestingDepositAccount depositAccount = VestingDepositAccount(depositAccountAddress);
-        depositAccount.init(address(token), address(this), _beneficiary);
-
-        // Create schedule
-        vestingSchedule[_beneficiary] = Schedule({
-            amount : _amount,
-            depositAccount : depositAccount
-            });
-
-        // Vest the tokens into the deposit account and delegate to the beneficiary
-        require(
-            token.transferFrom(msg.sender, address(depositAccount), _amount),
-            "VestingContract::createVestingSchedule: Unable to transfer tokens to VDA"
-        );
-
-        emit ScheduleCreated(_beneficiary, _amount);
-
-        return true;
-    }
-
-    /**
-     * @notice Draws down any vested tokens due
-     * @dev Must be called directly by the beneficiary assigned the tokens in the schedule
-     */
-    function drawDown() nonReentrant external returns (bool) {
-        return _drawDown(msg.sender);
-    }
-
-    /**
-     * @notice Updates a schedule beneficiary
-     * @notice Voids the old schedule and transfers remaining amount to new beneficiary via a new schedule
-     * @dev Only owner
-     * @param _currentBeneficiary beneficiary to be replaced
-     * @param _newBeneficiary beneficiary to vest remaining tokens to
-     */
-    function updateScheduleBeneficiary(address _currentBeneficiary, address _newBeneficiary) external {
-        require(msg.sender == owner, "VestingContract::updateScheduleBeneficiary: Only owner");
-
-        // retrieve existing schedule
-        Schedule memory schedule = vestingSchedule[_currentBeneficiary];
-        require(
-            schedule.amount > 0,
-            "VestingContract::updateScheduleBeneficiary: There is no schedule currently in flight"
-        );
-        require(_drawDown(_currentBeneficiary), "VestingContract::_updateScheduleBeneficiary: Unable to drawn down");
-
-        // the old schedule is now void
-        voided[_currentBeneficiary] = true;
-
-        // setup new schedule with the amount left after the previous beneficiary's draw down
-        vestingSchedule[_newBeneficiary] = Schedule({
-            amount : schedule.amount.sub(totalDrawn[_currentBeneficiary]),
-            depositAccount : schedule.depositAccount
-            });
-
-        vestingSchedule[_newBeneficiary].depositAccount.switchBeneficiary(_newBeneficiary);
+        return _createVestingSchedule(_beneficiary, _amount);
     }
 
     /**
@@ -758,6 +640,15 @@ contract VestingContract is CloneFactory, ReentrancyGuard {
         owner = _newOwner;
     }
 
+    /**
+     * @notice Draws down any vested tokens due
+     * @dev Must be called directly by the beneficiary assigned the tokens in the schedule
+     */
+    function drawDown() nonReentrant external returns (bool) {
+        return _drawDown(msg.sender);
+    }
+
+
     // Accessors
 
     /**
@@ -765,8 +656,8 @@ contract VestingContract is CloneFactory, ReentrancyGuard {
      * @dev Must be called directly by the beneficiary assigned the tokens in the schedule
      * @return _tokenBalance total balance proxied via the ERC20 token
      */
-    function tokenBalance() external view returns (uint256 _tokenBalance) {
-        return token.balanceOf(address(vestingSchedule[msg.sender].depositAccount));
+    function tokenBalance() external view returns (uint256) {
+        return token.balanceOf(address(this));
     }
 
     /**
@@ -775,28 +666,16 @@ contract VestingContract is CloneFactory, ReentrancyGuard {
      * @return _amount
      * @return _totalDrawn
      * @return _lastDrawnAt
-     * @return _drawDownRate
      * @return _remainingBalance
-     * @return _depositAccountAddress
      */
     function vestingScheduleForBeneficiary(address _beneficiary)
     external view
-    returns (
-        uint256 _amount,
-        uint256 _totalDrawn,
-        uint256 _lastDrawnAt,
-        uint256 _drawDownRate,
-        uint256 _remainingBalance,
-        address _depositAccountAddress
-    ) {
-        Schedule memory schedule = vestingSchedule[_beneficiary];
+    returns (uint256 _amount, uint256 _totalDrawn, uint256 _lastDrawnAt, uint256 _remainingBalance) {
         return (
-        schedule.amount,
+        vestedAmount[_beneficiary],
         totalDrawn[_beneficiary],
         lastDrawnAt[_beneficiary],
-        schedule.amount.div(end.sub(start)),
-        schedule.amount.sub(totalDrawn[_beneficiary]),
-        address(schedule.depositAccount)
+        vestedAmount[_beneficiary].sub(totalDrawn[_beneficiary])
         );
     }
 
@@ -814,16 +693,57 @@ contract VestingContract is CloneFactory, ReentrancyGuard {
      * @param _beneficiary beneficiary of the vested tokens
      * @return _remainingBalance tokens still due (and currently locked) from vesting schedule
      */
-    function remainingBalance(address _beneficiary) external view returns (uint256 _remainingBalance) {
-        Schedule memory schedule = vestingSchedule[_beneficiary];
-        return schedule.amount.sub(totalDrawn[_beneficiary]);
+    function remainingBalance(address _beneficiary) external view returns (uint256) {
+        return vestedAmount[_beneficiary].sub(totalDrawn[_beneficiary]);
+    }
+    
+    /**
+     * @notice Updates a schedule beneficiary
+     * @notice Voids the old schedule and transfers remaining amount to new beneficiary via a new schedule
+     * @dev Only owner
+     * @param _currentBeneficiary beneficiary to be replaced
+     * @param _newBeneficiary beneficiary to vest remaining tokens to
+    */
+    function updateScheduleBeneficiary(address _currentBeneficiary, address _newBeneficiary) external {
+        require(msg.sender == owner, "VestingContract::updateScheduleBeneficiary: Only owner");
+
+        require(
+            vestedAmount[_currentBeneficiary] > 0,
+            "VestingContract::updateScheduleBeneficiary: There is no schedule currently in flight"
+        );
+        require(_drawDown(_currentBeneficiary), "VestingContract::_updateScheduleBeneficiary: Unable to drawn down");
+
+        // the old schedule is now void
+        voided[_currentBeneficiary] = true;
+
+        // setup new schedule with the amount left after the previous beneficiary's draw down
+        vestedAmount[_newBeneficiary]=vestedAmount[_currentBeneficiary].sub(totalDrawn[_currentBeneficiary]);
     }
 
     // Internal
 
+    function _createVestingSchedule(address _beneficiary, uint256 _amount) internal returns (bool) {
+        require(_beneficiary != address(0), "VestingContract::createVestingSchedule: Beneficiary cannot be empty");
+        require(_amount > 0, "VestingContract::createVestingSchedule: Amount cannot be empty");
+
+        // Ensure one per address
+        require(vestedAmount[_beneficiary] == 0, "VestingContract::createVestingSchedule: Schedule already in flight");
+
+        vestedAmount[_beneficiary] = _amount;
+
+        // Vest the tokens into the deposit account and delegate to the beneficiary
+        require(
+            token.transferFrom(msg.sender, address(this), _amount),
+            "VestingContract::createVestingSchedule: Unable to escrow tokens"
+        );
+
+        emit ScheduleCreated(_beneficiary);
+
+        return true;
+    }
+
     function _drawDown(address _beneficiary) internal returns (bool) {
-        Schedule memory schedule = vestingSchedule[_beneficiary];
-        require(schedule.amount > 0, "VestingContract::_drawDown: There is no schedule currently in flight");
+        require(vestedAmount[_beneficiary] > 0, "VestingContract::_drawDown: There is no schedule currently in flight");
 
         uint256 amount = _availableDrawDownAmount(_beneficiary);
         require(amount > 0, "VestingContract::_drawDown: No allowance left to withdraw");
@@ -836,17 +756,14 @@ contract VestingContract is CloneFactory, ReentrancyGuard {
 
         // Safety measure - this should never trigger
         require(
-            totalDrawn[_beneficiary] <= schedule.amount,
+            totalDrawn[_beneficiary] <= vestedAmount[_beneficiary],
             "VestingContract::_drawDown: Safety Mechanism - Drawn exceeded Amount Vested"
         );
 
         // Issue tokens to beneficiary
-        require(
-            schedule.depositAccount.transferToBeneficiary(amount),
-            "VestingContract::_drawDown: Unable to transfer tokens"
-        );
+        require(token.transfer(_beneficiary, amount), "VestingContract::_drawDown: Unable to transfer tokens");
 
-        emit DrawDown(_beneficiary, amount, _getNow());
+        emit DrawDown(_beneficiary, amount);
 
         return true;
     }
@@ -856,22 +773,21 @@ contract VestingContract is CloneFactory, ReentrancyGuard {
     }
 
     function _availableDrawDownAmount(address _beneficiary) internal view returns (uint256 _amount) {
-        Schedule memory schedule = vestingSchedule[_beneficiary];
-
+        
         // voided contract should not allow any draw downs
         if (voided[_beneficiary]) {
             return 0;
         }
-
-        // cliff
+        
+        // Cliff Period
         if (_getNow() <= start.add(cliffDuration)) {
             // the cliff period has not ended, no tokens to draw down
             return 0;
         }
 
-        // schedule complete
+        // Schedule complete
         if (_getNow() > end) {
-            return schedule.amount.sub(totalDrawn[_beneficiary]);
+            return vestedAmount[_beneficiary].sub(totalDrawn[_beneficiary]);
         }
 
         // Schedule is active
@@ -883,7 +799,7 @@ contract VestingContract is CloneFactory, ReentrancyGuard {
         uint256 timePassedSinceLastInvocation = _getNow().sub(timeLastDrawnOrStart);
 
         // Work out how many due tokens - time passed * rate per second
-        uint256 drawDownRate = schedule.amount.div(end.sub(start));
+        uint256 drawDownRate = vestedAmount[_beneficiary].div(end.sub(start));
         uint256 amount = timePassedSinceLastInvocation.mul(drawDownRate);
 
         return amount;
